26d1b1a32e5a39dcfcbf2364e90dedc4ee310879
cse-aditya-maurya <cse.aditya.maurya@gmail.com>
2026-02-14 14:38:11 +0530

    Delete University_Course_Enrollment directory

--- University_Course_Enrollment/Faculty.java
+++
@@ -1,5 +0,0 @@
-class Faculty extends UniverSityManagment {
-	Faculty(int id,String name,String department,int yearOfExp){
-     	super(id,name,department,yearOfExp);
-     }
-}
--- University_Course_Enrollment/Student.java
+++
@@ -1,6 +0,0 @@
-class Student extends UniverSityManagment {
-     Student(int id,String name,String department,int yearOfExp){
-     	super(id,name,department,yearOfExp);
-     }
-
-}
